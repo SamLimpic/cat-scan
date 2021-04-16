e86579c389b9629a48be55cf7f32488667ecb8e8
davidparker83686 <davidjparker91@gmail.com>
2021-04-16 13:08:38 -0600

    cat in db

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 import ValueSchema from '../models/Value'
+import CatSchema from '../models/Cat'
 import AccountSchema from '../models/Account'
 
 class DbContext {
+  Cats = mongoose.model('Cat', CatSchema);
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
 }
