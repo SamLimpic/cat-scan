d36e14b27e646835ae32a7574919c9546b6f1d88
davidparker83686 <davidjparker91@gmail.com>
2021-04-16 14:36:43 -0600

    comment db

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -2,12 +2,14 @@ import mongoose from 'mongoose'
 import ValueSchema from '../models/Value'
 import CatSchema from '../models/Cat'
 import VoteSchema from '../models/Vote'
+import CommentSchema from '../models/Comment'
 import AccountSchema from '../models/Account'
 
 class DbContext {
   Votes = mongoose.model('Vote', VoteSchema);
   Cats = mongoose.model('Cat', CatSchema);
   Values = mongoose.model('Value', ValueSchema);
+  Comments = mongoose.model('Comment', CommentSchema);
   Account = mongoose.model('Account', AccountSchema);
 }
 
