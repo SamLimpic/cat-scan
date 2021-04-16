8c927d536259180686a85e91571697f5e42d077d
davidparker83686 <davidjparker91@gmail.com>
2021-04-16 15:50:32 -0600

    auth fix

--- server/models/Account.js
+++ server/models/Account.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const Account = new Schema(
   {
     email: { type: String, lowercase: true, unique: true },
-    userName: { type: String, required: true },
+    userName: { type: String },
     avatar: [{ type: String, unique: true }],
     _id: { type: String, required: true }
   },
