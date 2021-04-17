ce7890cdb6f47206faaf450fe42b0a5360f90121
davidparker83686 <davidjparker91@gmail.com>
2021-04-16 20:08:09 -0600

    cats tem in model

--- client/app/Models/Cat.js
+++ client/app/Models/Cat.js
@@ -10,7 +10,28 @@ export default class Cat {
 
   get Template() {
     return /* html */ `
-      <span>${this.body}, ${this.imgUrl}, ${this.voteCount}, ${this.posCount}, ${this.negCount}, ${this.accountId},</span>
+
+                <div class="card rounded col-11 col-md-3 p-3 shadow mt-3 m-5">
+                <div class=" ">
+
+                    <div class="row justify-content-between pr-3 pl-3">
+                        <span>o <b>${this.accountId}</b></span>
+                        <button type="button" class="btn btn-non shadow-none text-muted"><b>...</b></button>
+                    </div>
+
+                    <img class="img-size shadow rounded justify-content-center" src="${this.imgUrl}"
+                        alt="cat post">
+
+                    <p class="pt-2">${this.body}</p>
+
+                    <div class="row justify-content-between px-3">
+                        <img class="paw " style="cursor: pointer" src="./assets/img/bluepaw.png" alt="sad">
+                        <img class="paw" style="cursor: pointer" src="./assets/img/redpaw.png" alt="sad">
+                        <!-- <button type="button" class="btn shadow btn-primary"><b>+</b></button>
+                        <button type="button" class="btn shadow btn-danger"><b>-</b></button> -->
+                    </div>
+                </div>
+            </div>
         `
   }
 }
