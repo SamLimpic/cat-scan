d814d716a65b11411ce39dd2d7eac8d5a3eaa26b
davidparker83686 <davidjparker91@gmail.com>
2021-04-16 23:56:10 -0600

    "finalized"

--- client/app/Services/AxiosService.js
+++ client/app/Services/AxiosService.js
@@ -4,14 +4,14 @@ const base = window.location.host.includes('localhost') ? '//localhost:3000/' : 
 // @ts-ignore
 export const api = axios.create({
   baseURL: base,
-  timeout: 3000,
+  timeout: 30000,
   withCredentials: true
 })
 
 // @ts-ignore
 export const catFactApi = axios.create({
   baseURL: 'https://catfact.ninja/fact?max_length=75',
-  timeout: 3000
+  timeout: 30000
 })
 
 // not set up yet
